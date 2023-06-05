e4274961d4649f4688fb2b6b6b23295307d7a60e
sinhaG <grv.sinha@gmail.com>
2023-06-05 19:29:21 +0530

    Created the home page designe

---
+++ react/coding-viz/app/page.jsx
@@ -0,0 +1,23 @@
+const Home = () => {
+
+  return (
+
+    <section className="w-full flex-center flwx-col">
+        <h1 className="head_text text-center">
+    Discover @ Share
+
+<br className="max-md:hidden"/>
+<span className="orange_gradient text-center"> All the code Utilites</span>
+        </h1>
+
+        <p className="desc text-center">
+          ALL THE CODE WILL BE MAINTAINED HERE
+        </p>
+
+        {/* Feed  */}
+    </section>
+  )
+}
+
+
+export default Home
