7dc341b8860b2b6d579dbd8285d64ed928d5abc5
sinhaG <grv.sinha@gmail.com>
2023-06-08 11:50:28 +0530

    countdown  timer

---
+++ react/coding-viz/app/codeEntry/timer/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react';
+import Timer from './Timer';
+export default function Page() {
+    return (
+        <div>
+          <Timer/>
+        </div>
+      );
+}
